6c07efac9d0e382909b871b432856b4fe002d6eb
Mamta <mamtarajput925@gmail.com>
2022-06-07 09:33:53 +0530

    Create cred.js

---
+++ interview/cred.js
@@ -0,0 +1,15 @@
+Cred
+Design key store distributed cache system with modular, scalable system.
+I was asked a coding challenge that consisted of designing a key value store with support for multiple attributes and indexing functionality within 2 hours (pretty much like AWS DynamoDB), but the interviewer was too keen on abstraction.
+
+
+
+
+
+Object : sql
+
+
+
+Select * from extr where connectionId = “123124252352”
+
+{connectionId: “123124252352”}
