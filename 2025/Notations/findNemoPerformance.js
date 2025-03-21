b99fb1cfd18abc294beed6c2b504233adef0c0bf
makumari <mamta.kumari@booking.com>
2025-03-21 09:47:04 +0530

    DSA-1 performance by time

---
+++ 2025/Notations/findNemoPerformance.js
@@ -0,0 +1,18 @@
+const nemo = ['nemo'];
+
+const large = new Array(100000).fill('nemo')
+
+
+function findNemo(array){
+    let t0 = performance.now();
+    for(let i=0;i<array.length; i++){
+        if(array[i] === 'nemo'){
+            console.log('FOUND NEMO!')
+        }
+    }
+
+    let t1 = performance.now();
+    console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
+
+findNemo(large)
